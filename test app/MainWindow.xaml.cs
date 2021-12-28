223db25d78583cf54d2aaeacee71a635afcdeb22
Ronar8 <ronar432@gmail.com>
2021-12-28 01:04:40 +0100

    cleanup of repository, implemented math solving functionality, countdown timer and player lifes

---
+++ test app/MainWindow.xaml.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;
+using System.Threading;
+
+namespace test_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        DispatcherTimer stopwatch = new DispatcherTimer();
+        DispatcherTimer countdown = new DispatcherTimer();
+
+        ImageBrush playerSprite = new ImageBrush();
+        ImageBrush backgroundSprite = new ImageBrush();
+        ImageBrush obstacle1_Sprite = new ImageBrush();
+
+        ImageBrush obstacle2_Sprite = new ImageBrush();
+        ImageBrush obstacle3_Sprite = new ImageBrush();
+        ImageBrush obstacle4_Sprite = new ImageBrush();
+
+        Rect player_hitbox;
+        Rect ground_hitbox;
+        Rect obstacle1_hitbox;
+
+        Rect obstacle2_hitbox;
+        Rect obstacle3_hitbox;
+        Rect obstacle4_hitbox;
+
+        private static Random rand = new Random();
+
+        //variables regarding player character
+        bool jumping;
+        int jump_force;
+        int player_speed;
+        int lifes;
+        double sprite_index;
+
+        int timer;
+        int countdown_timer;
+
+        double diff_scrolling;
+        double diff_player_speed;
+
+        bool stop_game;
+
+        
+
+        //randomize obstacle height protruding from the ground
+        int [] obstacle_height = {300, 305, 310, 315, 320};
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyCanvas.Focus();
+
+            gameTimer.Tick += GameEngine;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+
+            stopwatch.Interval = TimeSpan.FromSeconds(1);
+            stopwatch.Tick += stopwatch_Tick;
+
+            countdown.Interval = TimeSpan.FromSeconds(1);
+            countdown.Tick += countdown_Tick;
+
+
+            InitializeGameElements();
+
+            countdown_timer = 3;
+
+            countdown_3s.Content = "3";
+
+            countdown.Start();
+        }
+
+        private void GameEngine(object sender, EventArgs e)
+        {
+
+            //scrolling player_speed of background
+            Canvas.SetLeft(background, Canvas.GetLeft(background) - diff_scrolling);
+            Canvas.SetLeft(background_2, Canvas.GetLeft(background_2) - diff_scrolling);
+
+            //parallax scrolling of background
+            if (Canvas.GetLeft(background) < -1262)
+            {
+                Canvas.SetLeft(background, Canvas.GetLeft(background_2) + background_2.Width - 1);
+            }
+
+            if (Canvas.GetLeft(background_2) < -1262)
+            {
+                Canvas.SetLeft(background_2, Canvas.GetLeft(background) + background.Width - 1);
+            }
+
+            //land the player on the ground
+            Canvas.SetTop(player, Canvas.GetTop(player) + player_speed);
+            Canvas.SetLeft(obstacle1, Canvas.GetLeft(obstacle1) - 12);
+
+            //setup hitboxes
+            player_hitbox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width - 25, player.Height - 5);
+            obstacle1_hitbox = new Rect(Canvas.GetLeft(obstacle1), Canvas.GetTop(obstacle1), obstacle1.Width, obstacle1.Height);
+            ground_hitbox = new Rect(Canvas.GetLeft(ground), Canvas.GetTop(ground), ground.Width, ground.Height);
+
+            //logic when player lands on ground
+            if (player_hitbox.IntersectsWith(ground_hitbox))
+            {
+                player_speed = 0;
+
+                Canvas.SetTop(player, Canvas.GetTop(ground) - player.Height);
+
+                jumping = false;
+
+                sprite_index += diff_player_speed;
+
+                //dynamic difficulty, scrolling speed and player sprite change speed
+                if (timer % 5 == 0 && timer != 0)
+                {
+                    diff_player_speed += 0.001;
+                    diff_scrolling += 0.1;
+                }
+
+                //repeat animation when running out of sprites (no more than 8)
+                if (sprite_index > 8)
+                {
+                    sprite_index = 1;
+                }
+
+                Sprite_Change(sprite_index);
+            }
+
+            if (player_hitbox.IntersectsWith(obstacle1_hitbox))
+            {
+                stop_game = true;
+
+                gameTimer.Stop();
+                stopwatch.Stop();
+
+                math_solving math_window = new math_solving();
+                math_window.ShowDialog();
+
+                if (math_window.DialogResult == true)
+                {
+                    Canvas.SetLeft(player, Canvas.GetLeft(player) + 100);
+
+                    stop_game = false;
+
+                    gameTimer.Start();
+                    stopwatch.Start();
+                }
+                else
+                {
+                    lifes--;
+
+                    if (lifes > 0)
+                    {
+                        Uri dynamic_hearts = new Uri("pack://application:,,,/lifes/hearts_" + lifes + ".png");
+                        life_hearts.Source = new BitmapImage(dynamic_hearts);
+
+                        Canvas.SetLeft(player, Canvas.GetLeft(player) + 100);
+
+                        stop_game = false;
+
+                        gameTimer.Start();
+                        stopwatch.Start();
+                    }
+                    else
+                    {
+                        life_hearts.Visibility = Visibility.Collapsed;
+
+                        stop_game = true;
+
+                        gameTimer.Stop();
+                        stopwatch.Stop();
+
+                        MessageBox.Show("Wyczerpano limit zyc, koniec gry");
+                    }
+
+                }
+            }
+
+            // limit jump range
+            if (jumping == true)
+            {
+                player_speed = -10;
+                jump_force -= 1;
+            }
+            else
+            {
+                player_speed = 12;
+            }
+
+            // if limit of jump height is reached, fall back down
+            if (jump_force < 0)
+            {
+                jumping = false;
+            }
+
+
+            // repeat obstacle1
+            if (Canvas.GetLeft(obstacle1) < -50)
+            {
+                Canvas.SetLeft(obstacle1, 950);
+
+                Canvas.SetTop(obstacle1, obstacle_height[rand.Next(0, obstacle_height.Length)]);
+            }
+
+        }
+
+        private void stopwatch_Tick(object sender, EventArgs e)
+        {
+            timer++;
+
+            Time.Content = "Czas: " + timer + " s";
+        }
+
+        private void countdown_Tick(object sender, EventArgs e)
+        {
+            if (countdown_timer > 0)
+            {
+                countdown_timer--;
+
+                countdown_3s.Content = countdown_timer;
+            }
+            else
+            {
+                countdown.Stop();
+                countdown_3s.Visibility = Visibility.Collapsed;
+                StartGame();
+            }
+
+
+        }
+
+        //two functions related to dynamic difficulty, not working properly
+        //temporary fix applied
+        /*private double time_difficulty_scrolling(double n)
+        {
+            if (timer % 5 == 0 && timer != 0)
+            {
+                return n + 0.1;
+            }
+            else
+            {
+                return n;
+            }
+        }
+
+        private double time_difficulty_player(double m)
+        {
+            if (timer % 5 == 0 && timer != 0)
+            {
+                return m + 0.01;
+            }
+            else
+            {
+                return m;
+            }
+        }*/
+
+        private void Key_Down(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.R && stop_game == true)
+            {
+                StartGame();
+                stopwatch.Start();
+            }
+        }
+
+        private void Key_Up(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space && jumping == false && Canvas.GetTop(player) > 260)
+            {
+                jumping = true;
+                jump_force = 15;
+                player_speed = -12;
+
+                playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/runner_02.gif"));
+            }
+        }
+
+        private void InitializeGameElements()
+        {
+            backgroundSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/background.gif"));
+            background.Fill = backgroundSprite;
+            background_2.Fill = backgroundSprite;
+
+            Canvas.SetLeft(background, 0);
+            Canvas.SetLeft(background_2, 1260);
+
+            Canvas.SetLeft(player, 110);
+            Canvas.SetTop(player, 230);
+
+            Canvas.SetLeft(obstacle1, 950);
+            Canvas.SetTop(obstacle1, 310);
+
+            Sprite_Change(1);
+
+            obstacle1_Sprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/obstacle.png"));
+            obstacle1.Fill = obstacle1_Sprite;
+
+            Uri default_hearts = new Uri("pack://application:,,,/lifes/hearts_3.png");
+            life_hearts.Source = new BitmapImage(default_hearts);
+        }
+
+        private void StartGame()
+        {
+            jumping = false;
+            stop_game = false;
+
+            jump_force = 20;
+            player_speed = 5;
+
+            timer = 0;
+            lifes = 3;
+
+            diff_scrolling = 8;
+            diff_player_speed = 0.5;
+
+            sprite_index = 0;
+
+            gameTimer.Start();
+            stopwatch.Start();
+        }
+
+        private void Sprite_Change(double i)
+        {
+            playerSprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/runner_0" + Math.Ceiling(i) + ".gif"));
+
+            player.Fill = playerSprite;
+        }
+    }
+}
---
+++ test app/math_solving.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace test_app
+{
+    /// <summary>
+    /// Logika interakcji dla klasy math_solving.xaml
+    /// </summary>
+    public partial class math_solving : Window
+    {
+
+        private static Random rnd = new Random();
+
+        int addend1;
+        int addend2;
+
+        double answer = 0;
+
+        int timeLeft;
+
+        DispatcherTimer math_timer = new DispatcherTimer();
+
+        public math_solving()
+        {
+            InitializeComponent();
+
+            math_timer.Interval = TimeSpan.FromSeconds(1);
+            math_timer.Tick += timer_Tick;
+
+            math_problem_generator();
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+                timeLabel.Content = timeLeft + " sekund";
+            }
+            else
+            {
+                math_timer.Stop();
+                timeLabel.Content = "Koniec czasu!";
+                MessageBox.Show("Wyczerpano limit czasu, tracisz 1 zycie");
+                math_answer.Text = answer.ToString();
+
+                this.DialogResult = false;
+            }
+        }
+
+        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            string input = math_answer.Text;
+
+            int num_input = Int16.Parse(input);
+
+            if (num_input == answer)
+            {
+                this.DialogResult = true;
+            }
+            else if (num_input != answer)
+            {
+                this.DialogResult = false;
+            }
+            else
+            {
+                this.DialogResult = false;
+            }
+        }
+
+        public void math_problem_generator()
+        {
+            timeLeft = 30;
+            timeLabel.Content = " 30 seconds";
+            math_timer.Start();
+
+            addend1 = rnd.Next(100);
+            addend2 = rnd.Next(100);
+
+            string string1 = addend1.ToString();
+            string string2 = addend2.ToString();
+
+            string merged_string = string1 + string2;
+
+            string operator_field = "";
+
+
+            int math_Operator = rnd.Next(5);
+            
+            switch(math_Operator)
+            {
+                case 1:
+                    operator_field = "+";
+                    answer = addend1 + addend2;
+                    break;
+                case 2:
+                    operator_field = "-";
+                    answer = addend1 - addend2;
+                    break;
+                case 3:
+                    operator_field = "*";
+                    answer = addend1 * addend2;
+                    break;
+                case 4:
+                    operator_field = "/";
+                    answer = addend1 / addend2;
+                    break;
+                default:
+                    operator_field = "+";
+                    answer = addend1 + addend2;
+                    break;
+            }
+
+
+
+
+            math_question.Content = "What is the result: " + string1 + operator_field + string2;
+
+        }
+
+    }
+}
